3078e846faa1ede61beab8f4255fc87f7692fdf0
Cesar Mejia <cmejia@gmail.com>
2023-03-20 16:35:30 +0700

    complete 29-32 ... 2 to go

--- src/06-challenges/29-infinite-scroll.solution.ts
+++ src/06-challenges/29-infinite-scroll.solution.ts
@@ -1,77 +1,75 @@
-import { expect, it } from "vitest";
-import { Equal, Expect } from "../helpers/type-utils";
+import { expect, it } from 'vitest'
+import { Equal, Expect } from '../helpers/type-utils'
 
 type MakeInfiniteScrollParams<TRow> = {
-  key: keyof TRow;
-  initialRows?: TRow[];
-  fetchRows: () => Promise<TRow[]> | TRow[];
-};
+  key: keyof TRow
+  initialRows?: TRow[]
+  fetchRows: () => Promise<TRow[]> | TRow[]
+}
 
 const makeInfiniteScroll = <TRow>(params: MakeInfiniteScrollParams<TRow>) => {
-  const data = params.initialRows || [];
+  const data = params.initialRows || []
 
   const scroll = async () => {
-    const rows = await params.fetchRows();
-    data.push(...rows);
-  };
+    const rows = await params.fetchRows()
+    data.push(...rows)
+  }
 
   return {
     scroll,
     getRows: () => data,
-  };
-};
+  }
+}
 
-it("Should fetch more data when scrolling", async () => {
+it('Should fetch more data when scrolling', async () => {
   const table = makeInfiniteScroll({
-    key: "id",
-    fetchRows: async () => [{ id: 1, name: "John" }],
-  });
+    key: 'id',
+    fetchRows: async () => [{ id: 1, name: 'John' }],
+  })
 
-  await table.scroll();
+  await table.scroll()
 
-  await table.scroll();
+  await table.scroll()
 
   expect(table.getRows()).toEqual([
-    { id: 1, name: "John" },
-    { id: 1, name: "John" },
-  ]);
-});
+    { id: 1, name: 'John' },
+    { id: 1, name: 'John' },
+  ])
+})
 
-it("Should ensure that the key is one of the properties of the row", () => {
+it('Should ensure that the key is one of the properties of the row', () => {
   makeInfiniteScroll({
     // @ts-expect-error
-    key: "name",
+    key: 'name',
     fetchRows: () =>
       Promise.resolve([
         {
-          id: "1",
+          id: '1',
         },
       ]),
-  });
-});
+  })
+})
 
-it("Should allow you to pass initialRows", () => {
+it('Should allow you to pass initialRows', () => {
   const { getRows } = makeInfiniteScroll({
-    key: "id",
+    key: 'id',
     initialRows: [
       {
         id: 1,
-        name: "John",
+        name: 'John',
       },
     ],
     fetchRows: () => Promise.resolve([]),
-  });
+  })
 
-  const rows = getRows();
+  const rows = getRows()
 
   expect(rows).toEqual([
     {
       id: 1,
-      name: "John",
+      name: 'John',
     },
-  ]);
+  ])
 
-  type tests = [
-    Expect<Equal<typeof rows, Array<{ id: number; name: string }>>>
-  ];
-});
+  type tests = [Expect<Equal<typeof rows, Array<{ id: number; name: string }>>>]
+})
