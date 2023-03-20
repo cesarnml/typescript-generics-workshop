cf2fd237986126263ef979205b8f6981789e66f4
Cesar Mejia <cmejia@gmail.com>
2023-03-20 13:51:15 +0700

    finished advance generic section

--- src/04-generics-advanced/20.6-reusable-context.problem.ts
+++ src/04-generics-advanced/20.6-reusable-context.problem.ts
@@ -1,4 +1,4 @@
-import { CSSProperties } from "react";
+import { CSSProperties } from 'react'
 
 /**
  * In this implementation, we need to specify the theme
@@ -12,26 +12,31 @@ import { CSSProperties } from "react";
  *
  * const useStyled = makeUseStyled<MyTheme>();
  */
-const useStyled = <TTheme = {}>(func: (theme: TTheme) => CSSProperties) => {
-  // Imagine that this function hooks into a global theme
-  // and returns the CSSProperties
-  return {} as CSSProperties;
-};
+const makeUseStyled = <TTheme>() => {
+  const useStyled = (func: (theme: TTheme) => CSSProperties) => {
+    // Imagine that this function hooks into a global theme
+    // and returns the CSSProperties
+    return {} as CSSProperties
+  }
+  return useStyled
+}
+
+const useStyled = makeUseStyled<MyTheme>()
 
 interface MyTheme {
   color: {
-    primary: string;
-  };
+    primary: string
+  }
   fontSize: {
-    small: string;
-  };
+    small: string
+  }
 }
 
-const buttonStyle = useStyled<MyTheme>((theme) => ({
+const buttonStyle = useStyled((theme) => ({
   color: theme.color.primary,
   fontSize: theme.fontSize.small,
-}));
+}))
 
-const divStyle = useStyled<MyTheme>((theme) => ({
+const divStyle = useStyled((theme) => ({
   backgroundColor: theme.color.primary,
-}));
+}))
